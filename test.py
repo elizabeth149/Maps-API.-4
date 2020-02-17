8c65bf6e2185f8080a17e495740513273eea999b
eliza_b_e_t_h <gllizaveta@mail.ru>
2020-02-17 18:51:27 +0300

    выбор карты

--- test.py
+++ test.py
@@ -4,15 +4,17 @@ import pygame
 
 address_ll = float(sys.argv[1])
 address_ll_2 = float(sys.argv[2])
+vid = sys.argv[3]
 response = None
 
 
 def sizze():
     global address_ll
     global address_ll_2
+    global vid
     global response
     sp = f"https://static-maps.yandex.ru/1.x/?ll={address_ll},{address_ll_2}" \
-         f"&z=5&l=sat"
+         f"&z=5&l={vid}"
     response = requests.get(sp)
     map_file = "map.png"
     with open(map_file, "wb") as file:
@@ -33,17 +35,17 @@ while running:
         keys = pygame.key.get_pressed()
         if keys[pygame.K_UP]:
             if 0 <= address_ll_2 + 1 <= 180:
-                address_ll_2 += 0.1
+                address_ll_2 += 0.5
                 sizze()
         if keys[pygame.K_DOWN]:
             if 0 <= address_ll_2 - 1 <= 180:
-                address_ll_2 -= 0.1
+                address_ll_2 -= 0.5
                 sizze()
         if keys[pygame.K_LEFT]:
             if 0 <= address_ll - 1 <= 180:
-                address_ll -= 0.1
+                address_ll -= 0.5
                 sizze()
         if keys[pygame.K_RIGHT]:
             if 0 <= address_ll + 1 <= 180:
-                address_ll += 0.1
+                address_ll += 0.5
                 sizze()
